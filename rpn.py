86ca977f96c0b38f53e910509f424aec267cc3ef
ehcole <ehcole@umich.edu>
2018-10-26 15:01:16 -0400

    passed test1

--- rpn.py
+++ rpn.py
@@ -1,10 +1,24 @@
 #!/usr/bin/env python3
-def calculate(arg):
-    pass
 
+def calculate(arg):
+    stack = []
+    tokens = arg.split(" ")
+    for token in tokens:
+        try:
+            stack.append(int(token))
+        except ValueError:
+            val1 = stack.pop()
+            val2 = stack.pop()
+            result = val1 + val2
+            stack.append(result)
+    return stack[0]
+    
+            
+    
 def main():
     while True:
-        calculate(input("rpn calc> "))
+        print(calculate(input("rpn calc> ")))
+        
 
 if __name__ == '__main__':
     main()
