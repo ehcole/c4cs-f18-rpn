13b85728c3598bb85cd0ee059dd2441e9a06b56b
ehcole <ehcole@umich.edu>
2018-11-06 15:14:49 -0500

    added pretty colors

--- rpn.py
+++ rpn.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
 import operator
-
+import readline
+import colorama
 
 operators = {
         '+': operator.add,
@@ -9,8 +10,8 @@ operators = {
         '*': operator.mul,
         '/': operator.truediv,
         '^': operator.pow,
+        'q': "quit"
     }
-
 def calculate(myarg):
     stack = list()
     for token in myarg.split():
@@ -18,12 +19,25 @@ def calculate(myarg):
             token = int(token)
             stack.append(token)
         except ValueError:
+            if token == "q":
+                exit(0)
             function = operators[token]
             arg2 = stack.pop()
             arg1 = stack.pop()
             result = function(arg1, arg2)
             stack.append(result)
-        print(stack)
+#            stack.append('^')
+        for item in stack:
+            try:
+                num = int(item)
+            except:
+                print(colorama.Back.GREEN + item)
+            else:
+                if num < 0:
+                    print(colorama.Style.BRIGHT + colorama.Fore.RED + str(num))
+                else:
+                    print(str(num))
+            print(colorama.Style.RESET_ALL)
     if len(stack) != 1:
         raise TypeError("Too many parameters")
     return stack.pop()
@@ -31,7 +45,11 @@ def calculate(myarg):
 def main():
     while True:
         result = calculate(input("rpn calc> "))
-        print("Result: ", result)
+        if result < 0:
+            print("Result: ", colorama.Style.BRIGHT + colorama.Fore.RED + str(result))
+            print(colorama.Style.RESET_ALL)
+        else:
+            print("Result: ", result)
         
 if __name__ == '__main__':
     main()
